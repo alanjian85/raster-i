5b81368a87023fc14b957fe92bec7aeebe6e5222
Alan Jian <alanjian85@outlook.com>
2024-06-25 23:21:20 +0800

    Add normal vector data of Suzanne

--- renderer/include/mesh.hpp
+++ renderer/include/mesh.hpp
@@ -2,12 +2,15 @@
 
 #include <math/vec.hpp>
 
-struct Vertex {
-    Vec3f pos;
+struct MeshIndex {
+    Vec3i vertices;
+    Vec3i normals;
 };
 
 #define NR_MESH_VERTICES 507
+#define NR_MESH_NORMALS 944
 #define NR_MESH_TRIANGLES 968
 
-extern const Vertex MESH_VERTICES[NR_MESH_VERTICES];
-extern const int MESH_INDICES[3 * NR_MESH_TRIANGLES];
+extern const Vec3f MESH_VERTICES[NR_MESH_VERTICES];
+extern const Vec3f MESH_NORMALS[NR_MESH_NORMALS];
+extern const MeshIndex MESH_INDICES[NR_MESH_TRIANGLES];
--- renderer/src/top.cpp
+++ renderer/src/top.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <cfloat>
-
 #include <cstdlib>
+
 #include <fb.hpp>
 #include <math/math.hpp>
 #include <math/triangle.hpp>
@@ -12,43 +12,50 @@
 #include <utils/color.hpp>
 
 static Vec2i transformed_vertices[NR_MESH_VERTICES];
+static float transformed_depths[NR_MESH_VERTICES];
 static Aabb2i bounding_boxes[NR_MESH_TRIANGLES];
 
-static Vertex interpolate_vertices(int i, Vec3f bary) {
-    int idx0 = MESH_INDICES[i * 3];
-    int idx1 = MESH_INDICES[i * 3 + 1];
-    int idx2 = MESH_INDICES[i * 3 + 2];
+static float interpolate_depth(int i, Vec3f bary) {
+    Vec3i idx = MESH_INDICES[i].vertices;
 
-    Vertex vertex_a = MESH_VERTICES[idx0];
-    Vertex vertex_b = MESH_VERTICES[idx1];
-    Vertex vertex_c = MESH_VERTICES[idx2];
+    float z_a = transformed_depths[idx.x];
+    float z_b = transformed_depths[idx.y];
+    float z_c = transformed_depths[idx.z];
 
-    Vertex vertex;
-    // vertex.uv =
-    //     vertex_a.uv * bary.x + vertex_b.uv * bary.y + vertex_c.uv * bary.z;
-    return vertex;
+    return bary.x * z_a + bary.y * z_b + bary.z * z_c;
 }
 
-static void render_triangle(uint32_t *tile, Vec2i pos, int i) {
-    int idx0 = MESH_INDICES[i * 3 + 0];
-    int idx1 = MESH_INDICES[i * 3 + 1];
-    int idx2 = MESH_INDICES[i * 3 + 2];
-    Triangle2i triangle(transformed_vertices[idx0], transformed_vertices[idx1],
-                        transformed_vertices[idx2]);
+static Vec3f interpolate_normal(int i, Vec3f bary) {
+    Vec3i idx = MESH_INDICES[i].normals;
+    Vec3f normal_a = MESH_NORMALS[idx.x];
+    Vec3f normal_b = MESH_NORMALS[idx.y];
+    Vec3f normal_c = MESH_NORMALS[idx.z];
+    return bary.x * normal_a + bary.y * normal_b + bary.z * normal_c;
+}
+
+static void render_triangle(uint32_t *tile, float *zbuf, Vec2i pos, int i) {
+    Vec3i idx = MESH_INDICES[i].vertices;
+    Triangle2i triangle(transformed_vertices[idx.x],
+                        transformed_vertices[idx.z],
+                        transformed_vertices[idx.y]);
 
 render_y:
     for (int y = 0; y < FB_TILE_WIDTH; y++) {
     render_x:
         for (int x = 0; x < FB_TILE_HEIGHT; x++) {
-#pragma HLS PIPELINE rewind
 #pragma HLS UNROLL factor = 8
 #pragma HLS ARRAY_PARTITION variable = tile type = cyclic factor = 8
             std::pair<bool, Vec3f> bary =
                 triangle.barycentric(Vec2i(pos.x + x, pos.y + y));
-            if (bary.first) {
-                // Vertex vertex = interpolate_vertices(i, bary.second);
-                tile[y * FB_TILE_WIDTH + x] = 0xFFFFFFFF;
-                // sample_texture(vertex.uv).encode();
+            float z = interpolate_depth(i, bary.second);
+            if (bary.first && z <= zbuf[y * FB_TILE_WIDTH + x]) {
+                Vec3f normal = interpolate_normal(i, bary.second);
+                RGB8 rgb;
+                rgb.r = (normal.x + 1) * 0.5 * 255;
+                rgb.g = (normal.y + 1) * 0.5 * 255;
+                rgb.b = (normal.z + 1) * 0.5 * 255;
+                tile[y * FB_TILE_WIDTH + x] = rgb.encode();
+                zbuf[y * FB_TILE_WIDTH + x] = z;
             }
         }
     }
@@ -66,7 +73,7 @@ void trinity_renderer(fb_id_t fb_id, hls::burst_maxi<ap_uint<128>> vram,
 preproc_vertices:
     for (int i = 0; i < NR_MESH_VERTICES; i++) {
 #pragma HLS PIPELINE off
-        Vec3f pos = MESH_VERTICES[i].pos;
+        Vec3f pos = MESH_VERTICES[i];
         Vec3f vc = axis * dot(pos, axis);
         Vec3f v1 = pos - vc;
         Vec3f v2 = cross(v1, axis);
@@ -75,17 +82,16 @@ preproc_vertices:
         transformed_vertices[i] =
             Vec2i((1 + pos.x / pos.z * 0.75f) * FB_WIDTH / 2,
                   (1 - pos.y / pos.z) * FB_HEIGHT / 2);
+        transformed_depths[i] = pos.z;
     }
 
 preproc_triangles:
     for (int i = 0; i < NR_MESH_TRIANGLES; i++) {
 #pragma HLS PIPELINE off
-        int idx0 = MESH_INDICES[i * 3];
-        int idx1 = MESH_INDICES[i * 3 + 1];
-        int idx2 = MESH_INDICES[i * 3 + 2];
-        Triangle2i triangle(transformed_vertices[idx0],
-                            transformed_vertices[idx1],
-                            transformed_vertices[idx2]);
+        Vec3i idx = MESH_INDICES[i].vertices;
+        Triangle2i triangle(transformed_vertices[idx.x],
+                            transformed_vertices[idx.y],
+                            transformed_vertices[idx.z]);
         bounding_boxes[i] = triangle.aabb();
     }
 
@@ -96,16 +102,18 @@ render_tile_y:
             Aabb2i aabb(Vec2i(x, y),
                         Vec2i(x + FB_TILE_WIDTH, y + FB_TILE_HEIGHT));
             uint32_t tile[FB_TILE_WIDTH * FB_TILE_HEIGHT];
+            float zbuf[FB_TILE_WIDTH * FB_TILE_HEIGHT];
         clear_tile:
             for (int i = 0; i < FB_TILE_WIDTH * FB_TILE_HEIGHT; i++) {
 #pragma HLS PIPELINE off
                 tile[i] = 0xFF000000;
+                zbuf[i] = FLT_MAX;
             }
 
         render_triangles:
             for (int i = 0; i < NR_MESH_TRIANGLES; i++) {
                 if (bounding_boxes[i].overlap(aabb)) {
-                    render_triangle(tile, Vec2i(x, y), i);
+                    render_triangle(tile, zbuf, Vec2i(x, y), i);
                 }
             }
 
