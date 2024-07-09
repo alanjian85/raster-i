10a1504e2505301abe1af8e7c5f2156ecb184665
Alan Jian <alanjian85@outlook.com>
2024-07-09 23:19:54 +0800

    Reduce the number of vertices

--- renderer/include/mesh.hpp
+++ renderer/include/mesh.hpp
@@ -8,9 +8,9 @@ struct MeshIndex {
     Vec3i normals;
 };
 
-#define NR_MESH_VERTICES 5000
-#define NR_MESH_NORMALS 9995
-#define NR_MESH_TRIANGLES 9996
+#define NR_MESH_VERTICES 1501
+#define NR_MESH_NORMALS 2998
+#define NR_MESH_TRIANGLES 2998
 
 extern const Vec3f MESH_VERTICES[NR_MESH_VERTICES];
 extern const RGB8 MESH_NORMALS[NR_MESH_NORMALS];
