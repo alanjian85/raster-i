8916c63295916f76c36b93defd05a86ed19c9b14
Alan Jian <alanjian85@outlook.com>
2024-10-03 23:44:42 +0800

    Increase texture resolution

--- renderer/include/texture.hpp
+++ renderer/include/texture.hpp
@@ -6,19 +6,13 @@
 #include <math/vec.hpp>
 #include <utils/color.hpp>
 
-#define TEXTURE_WIDTH 64
-#define TEXTURE_HEIGHT 64
+#define TEXTURE_WIDTH 256
+#define TEXTURE_HEIGHT 256
 
 extern const uint32_t TEXTURE[TEXTURE_WIDTH * TEXTURE_HEIGHT];
 
 inline RGB8 sample_texture(Vec2f uv) {
     int x = ceil(uv.x * (TEXTURE_WIDTH - 1));
     int y = ceil(uv.y * (TEXTURE_HEIGHT - 1));
-
-    RGB8 p00 = RGB8::decode(TEXTURE[y * TEXTURE_WIDTH + x]);
-    RGB8 p01 = RGB8::decode(TEXTURE[y * TEXTURE_WIDTH + x + 1]);
-    RGB8 p10 = RGB8::decode(TEXTURE[(y + 1) * TEXTURE_WIDTH + x]);
-    RGB8 p11 = RGB8::decode(TEXTURE[(y + 1) * TEXTURE_WIDTH + x + 1]);
-
-    return (p00 + p01 + p10 + p11) >> 2;
+    return RGB8::decode(TEXTURE[y * TEXTURE_WIDTH + x]);
 }
